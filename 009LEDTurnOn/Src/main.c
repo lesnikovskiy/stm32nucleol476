a58a88aa9e9779390a6d14473736037ba80c8b97
lesnikovski <lesnikovski@gmail.com>
2026-04-13 19:07:50 +0300

    Simple project to just turn LED on

---
+++ 009LEDTurnOn/Src/main.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+
+int main(void) {
+	uint32_t *gpioa_peripheral = (uint32_t*) 0x4002104C;
+	uint32_t *gpioa_moder = (uint32_t*) 0x48000000;
+	uint32_t *gpioa_data = (uint32_t*) 0x48000014;
+	//uint32_t *gpioa_bsrr = (uint32_t*) 0x48000018;
+
+	// 1. Enable clock for GPIOA Peripheral
+	*gpioa_peripheral |= (1 << 0);
+
+	// 2. Configure a mode of PA5 as Output
+	*gpioa_moder &= ~(3 << 10);
+	*gpioa_moder |= (1 << 10);
+
+	// 3. Put data to data register
+	*gpioa_data &= ~(1 << 5);
+	*gpioa_data |= (1 << 5);
+	//*gpioa_bsrr |= (1 << 5);
+
+	/* Loop forever */
+	for (;;);
+}
