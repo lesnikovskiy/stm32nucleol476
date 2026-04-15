b9dfb699d85a1665fe3a025201b9651860d11169
lesnikovski <lesnikovski@gmail.com>
2026-04-15 23:05:02 +0300

    Rename registers to upper case as const and according to naming convention

--- 009LEDTurnOn/Src/main.c
+++ 009LEDTurnOn/Src/main.c
@@ -1,26 +1,26 @@
 #include <stdint.h>
 
 int main(void) {
-	volatile uint32_t *const gpioa_peripheral = (volatile uint32_t*) 0x4002104C;
-	volatile uint32_t *const gpioa_moder = (volatile uint32_t*) 0x48000000;
-	volatile uint32_t *const gpioa_data = (volatile uint32_t*) 0x48000014;
-//	volatile uint32_t *const gpioa_bsrr = (volatile uint32_t*) 0x48000018;
+	volatile uint32_t *const AHB2_ENR = (volatile uint32_t*) 0x4002104C;
+	volatile uint32_t *const GPIOA_MODER = (volatile uint32_t*) 0x48000000;
+	volatile uint32_t *const GPIOA_ODR = (volatile uint32_t*) 0x48000014;
+//	volatile uint32_t *const GPIOA_BSRR = (volatile uint32_t*) 0x48000018;
 
 	// 1. Enable clock for GPIOA Peripheral
-	*gpioa_peripheral |= (1 << 0);
+	*AHB2_ENR |= (1 << 0);
 
 	// 2. Configure a mode of PA5 as Output
-	*gpioa_moder &= ~(3 << 10);
-	*gpioa_moder |= (1 << 10);
+	*GPIOA_MODER &= ~(3 << 10);
+	*GPIOA_MODER |= (1 << 10);
 
 	// 3. Put data to data register
-	*gpioa_data &= ~(1 << 5);
-	*gpioa_data |= (1 << 5);
-	//*gpioa_bsrr |= (1 << 5);
+	*GPIOA_ODR &= ~(1 << 5);
+	*GPIOA_ODR |= (1 << 5);
+//	*GPIOA_BSRR |= (1 << 5);
 
 	/* Loop forever */
 	for (;;) {
-		*gpioa_data ^= (1 << 5);
+		*GPIOA_ODR ^= (1 << 5);
 		for (volatile uint32_t i = 0; i < 400000; i++);
 	}
 }
